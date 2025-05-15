6d2b799aebcea2aadb099fba84df339ad6f830cb
WadeHuang1993 <a2335825@gmail.com>
2025-05-15 21:46:33 +0800

    feat:FAQ 自動把超連結轉成可以點擊的連結

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -18,8 +18,30 @@ class FaqController extends Controller
     public function frontIndex()
     {
         $faqs = Faq::orderBy('order')->get();
+        $faqs = $faqs->map(function (Faq $faq) {
+            $faq->answer = $this->autoLink($faq->answer);
+            return $faq;
+        });
         $seoData = $this->seoService->getFaqSeoData($faqs);
-        
+
         return view('faq.index', compact('faqs', 'seoData'));
     }
-} 
+
+    private function autoLink($text)
+    {
+        // 先處理 email
+        $text = preg_replace(
+            '/([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/',
+            '<a href="mailto:$1" class="underline text-[var(--primary-color)]">$1</a>',
+            $text
+        );
+
+        // 再處理網址
+        $text = preg_replace(
+            '/(https?:\/\/[\w\-\.\/\?\#\=\&\%]+)/i',
+            '<a href="$1" target="_blank" class="underline text-[var(--primary-color)]">$1</a>',
+            $text
+        );
+        return $text;
+    }
+}
