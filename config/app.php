bba43808ae582641e49bab5373b934ef61bb1a53
WadeHuang1993 <a2335825@gmail.com>
2025-05-15 22:40:45 +0800

    fix:修正 slack 通知 url

--- config/app.php
+++ config/app.php
@@ -16,6 +16,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'slack-new-appointment' => env('SLACK_NEW_APPOINTMENT', ''),
 
     /*
     |--------------------------------------------------------------------------
