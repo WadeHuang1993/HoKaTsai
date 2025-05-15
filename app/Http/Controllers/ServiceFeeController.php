7016b7300e02e5d1d093940d0f257ba5177548e9
WadeHuang1993 <a2335825@gmail.com>
2025-05-15 10:05:05 +0800

    feat:收費標準頁 SEO 優化
    issue #56

--- app/Http/Controllers/ServiceFeeController.php
+++ app/Http/Controllers/ServiceFeeController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\ServiceFee;
 use App\Models\TeamMember;
+use App\Services\SeoService;
 
 class ServiceFeeController extends Controller
 {
+    protected $seoService;
+    public function __construct(SeoService $seoService)
+    {
+        $this->seoService = $seoService;
+    }
     public function index()
     {
         $fees = ServiceFee::orderBy('order')->get();
-        return view('service_fees.index', compact('fees'));
+        $seoData = $this->seoService->getServiceFeeSeoData($fees);
+        return view('service_fees.index', compact('fees', 'seoData'));
     }
 }
